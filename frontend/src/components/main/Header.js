5ef0db7f65daaefe408bd8292da345239045dbb5
Your Name <you@example.com>
2021-01-12 12:48:53 +0100

    test

--- frontend/src/components/main/Header.js
+++ frontend/src/components/main/Header.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import Router from 'next/router';
-import NProgress from 'nprogress';
 
-Router.onRouteChangeStart = () => {
-  NProgress.start();
-};
-Router.onRouteChangeComplete = () => {
-  NProgress.done();
-};
-
-Router.onRouteChangeError = () => {
-  NProgress.done();
-};
 const Header = () => {
   return <div />;
 };
