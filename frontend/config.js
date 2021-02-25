24e54ac5814e7f1c34631e0818f188ceeddf3a57
Your Name <you@example.com>
2021-02-25 22:31:39 +0100

    prod

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `https://mtb-yoga-prod.herokuapp.com`;
+export const prodEndpoint = `https://mtb-backend-prod.herokuapp.com/`;
