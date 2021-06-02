23304c672b54894dab3885805d31304832cb30f0
Your Name <you@example.com>
2021-06-02 15:20:33 +0200

    reorganized

--- frontend2/config.js
+++ frontend2/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `https://mtb-backend-prod.herokuapp.com/`;
+export const prodEndpoint = `https://mtb-prod-yoga.herokuapp.com/`;
